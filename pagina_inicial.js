a474aec89d1dcd0bd58866c638f7da188dc0cb68
David Rigo Bressan <davidbressan35@gmail.com>
2023-12-08 13:08:48 -0400

    Implementação do JS no Projeto

---
+++ pagina_inicial.js
@@ -0,0 +1,23 @@
+
+const metade2 = document.getElementById('metade2');
+const email = document.getElementById('email');
+const senha = document.getElementById("senha");
+const div = document.querySelectorAll('.respostaemail');
+
+
+function emailValidate(){
+    var  testeemail = document.getElementById('email').value;
+    var testesenha = document.getElementById('senha').value;
+    if(testeemail  == ""){
+        alert('Digite um Email válido'); 
+    }
+    if(testesenha == ""){
+        alert('Digite uma Seha válida');
+     
+    }
+    
+    else {
+        window.location.href = "pagina_comercial.html"
+    }
+    
+};
